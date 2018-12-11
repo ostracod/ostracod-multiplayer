ba5717242072b407616e075d77108546d7af6b6a
John Eisenmann <esperantanaso@gmail.com>
2018-12-10 22:10:31 -0500

    Account authentication

--- pageUtils.js
+++ pageUtils.js
@@ -3,6 +3,8 @@ var fs = require("fs");
 var pathUtils = require("path");
 var Mustache = require("mustache");
 
+var ostracodMultiplayer = require("./ostracodMultiplayer").ostracodMultiplayer;
+
 function PageUtils() {
     
 }
@@ -65,6 +67,64 @@ PageUtils.prototype.reportDatabaseErrorWithPage = function(error, req, res) {
     var tempUrl = pageUtils.generateReturnUrl(req);
     pageUtils.serveMessagePage(res, "An error occurred. Please contact an administrator.", tempUrl.url, tempUrl.urlLabel);
     console.log(error);
+}
+
+PageUtils.prototype.reportDatabaseError = function(error, errorOutput, req, res) {
+    console.log(error);
+    if (errorOutput == pageUtils.errorOutput.JSON_ERROR_OUTPUT) {
+        pageUtils.reportDatabaseErrorWithJson(error, req, res);
+    }
+    if (errorOutput == pageUtils.errorOutput.PAGE_ERROR_OUTPUT) {
+        pageUtils.reportDatabaseErrorWithPage(error, req, res);
+    }
+}
+
+PageUtils.prototype.isAuthenticated = function(req) {
+    if (ostracodMultiplayer.mode == "development") {
+        if (!req.session.username) {
+            var tempUsername = req.query.username;
+            if (tempUsername) {
+                req.session.username = tempUsername;
+            }
+        }
+    }
+    return (typeof req.session.username !== "undefined");
+}
+
+PageUtils.prototype.checkAuthentication = function(errorOutput) {
+    if (errorOutput == pageUtils.errorOutput.SOCKET_ERROR_OUTPUT) {
+        return function(ws, req, next) {
+            if (pageUtils.isAuthenticated(req)) {
+                next();
+            } else {
+                ws.on("message", function(message) {
+                    ws.send(JSON.stringify({
+                        success: false,
+                        message: "You are not currently logged in."
+                    }));
+                });
+            }
+        };
+    } else {
+        return function(req, res, next) {
+            if (pageUtils.isAuthenticated(req)) {
+                next();
+            } else {
+                if (errorOutput == pageUtils.errorOutput.JSON_ERROR_OUTPUT) {
+                    res.json({success: false, message: "You are not currently logged in."});
+                }
+                if (errorOutput == pageUtils.errorOutput.PAGE_ERROR_OUTPUT) {
+                    pageUtils.serveMessagePage(res, "You must be logged in to view that page.", "login", "Log In");
+                }
+            }
+        };
+    }
+}
+
+PageUtils.prototype.errorOutput = {
+    JSON_ERROR_OUTPUT: 0,
+    PAGE_ERROR_OUTPUT: 1,
+    SOCKET_ERROR_OUTPUT: 2
 }
 
 
--- router.js
+++ router.js
@@ -10,6 +10,19 @@ var pageUtils = require("./pageUtils").pageUtils;
 var dbUtils = require("./dbUtils").dbUtils;
 var accountUtils = require("./accountUtils").accountUtils;
 
+var checkAuthentication = pageUtils.checkAuthentication;
+var JSON_ERROR_OUTPUT = pageUtils.errorOutput.JSON_ERROR_OUTPUT;
+var PAGE_ERROR_OUTPUT = pageUtils.errorOutput.PAGE_ERROR_OUTPUT;
+var SOCKET_ERROR_OUTPUT = pageUtils.errorOutput.SOCKET_ERROR_OUTPUT;
+
+router.get("/", function(req, res, next) {
+    if (pageUtils.isAuthenticated(req)) {
+        res.redirect("menu");
+    } else {
+        res.redirect("login");
+    }
+});
+
 router.get("/login", function(req, res, next) {
     pageUtils.renderPage(
         res,
@@ -17,6 +30,47 @@ router.get("/login", function(req, res, next) {
         ["javascript/login.js"],
         {author: ostracodMultiplayer.serverConfig.author}
     );
+});
+
+router.post("/loginAction", function(req, res, next) {
+    var tempUsername = req.body.username;
+    var tempPassword = req.body.password;
+    dbUtils.performTransaction(function(done) {
+        accountUtils.getAccountByUsername(tempUsername, function(error, result) {
+            if (error) {
+                pageUtils.reportDatabaseErrorWithJson(error, req, res);
+                done();
+                return;
+            }
+            if (!result) {
+                res.json({success: false, message: "Bad account credentials."});
+                done();
+                return;
+            }
+            accountUtils.comparePasswordWithHash(tempPassword, result.passwordHash, function(result) {
+                if (!result.success) {
+                    pageUtils.reportDatabaseErrorWithJson(result.error, req, res);
+                    done();
+                    return;
+                }
+                if (!result.isMatch) {
+                    res.json({success: false, message: "Bad account credentials."});
+                    done();
+                    return;
+                }
+                req.session.username = tempUsername;
+                res.json({success: true});
+                done();
+            });
+        });
+    }, function() {});
+});
+
+router.get("/logoutAction", function(req, res, next) {
+    if (req.session.username) {
+        delete req.session["username"];
+    }
+    res.redirect("login");
 });
 
 router.get("/createAccount", function(req, res, next) {
@@ -50,8 +104,8 @@ router.post("/createAccountAction", function(req, res, next) {
             }
             accountUtils.generatePasswordHash(tempPassword, function(result) {
                 if (!result.success) {
-                    accountUtils.releaseLock();
                     pageUtils.reportDatabaseErrorWithJson(result.error, req, res);
+                    done();
                     return;
                 }
                 var tempPasswordHash = result.hash;
@@ -71,6 +125,19 @@ router.post("/createAccountAction", function(req, res, next) {
             });
         });
     }, function() {});
+});
+
+router.get("/menu", checkAuthentication(PAGE_ERROR_OUTPUT), function(req, res, next) {
+    tempUsername = req.session.username;
+    pageUtils.renderPage(
+        res,
+        pageUtils.getLocalViewPath("menu.html"),
+        [],
+        {
+            username: tempUsername,
+            score: 0 // TODO: Populate score.
+        }
+    );
 });
 
 
